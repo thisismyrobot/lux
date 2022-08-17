f0405ee8f50417d48752f009cd2b950a81840fe0
Robert <rwallhead@gmail.com>
2022-08-17 21:05:58 +1000

    Preparing some libraries

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ A custom lux meter.
 ## Hat tips
 
 https://www.instructables.com/ESP8266-Pro-Tips/
+
 https://www.instructables.com/ESP-12F-ESP8266-Module-Minimal-Breadboard-for-Flas/
+
+https://randomnerdtutorials.com/esp8266-nodemcu-bh1750-ambient-light-sensor/
--- src/src.ino
+++ src/src.ino
@@ -2,9 +2,12 @@
  *  Add board manager URL: http://arduino.esp8266.com/stable/package_esp8266com_index.json
  *  Add board via board manager: esp8266
  *  Select this ESP8266 board: Generic ESP8266 Module
+ *  Install Libraries:
+ *      - BH1750 v1.3.0
 */
 #include <ESP8266WiFi.h>
-
+#include <Wire.h>
+#include <BH1750.h>
 
 void setup() {
     // Hat tip https://www.instructables.com/ESP8266-Pro-Tips/
@@ -17,7 +20,7 @@ void setup() {
 
 void loop() {
     digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    delay(250);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(2000);
+    delay(3000);
 }
