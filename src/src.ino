f523ee6f353853bc2e094a5746783d29d239d32f
Robert <rwallhead@gmail.com>
2022-08-20 09:24:24 +1000

    Working lux reading from the sensor

--- src/src.ino
+++ src/src.ino
@@ -9,6 +9,12 @@
 #include <Wire.h>
 #include <BH1750.h>
 
+// GPIO numbers
+#define SDA_PIN 4  // GPOI4/Pin 19/D2
+#define SCL_PIN 5  // GPIO5/Pin 20/D1
+
+BH1750 lightMeter;
+
 void setup() {
     WiFi.mode(WIFI_OFF);
     WiFi.forceSleepBegin();
@@ -17,6 +23,9 @@ void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
 
     Serial.begin(115200);
+
+    Wire.begin(SDA_PIN, SCL_PIN);
+    lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE_2);
 }
 
 void loop() {
@@ -24,6 +33,11 @@ void loop() {
     delay(250);
     digitalWrite(LED_BUILTIN, HIGH);
     delay(3000);
+
+    float lux = lightMeter.readLightLevel();
+    Serial.print("Light: ");
+    Serial.print(lux);
+    Serial.println(" lx");
 
     // The Office: Traveling Salesman :D
     Serial.println("Hi!");
